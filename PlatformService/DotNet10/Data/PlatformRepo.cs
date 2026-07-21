f1edbaad7708d94bfad8e7a2780c94d63f051e61
run1206 <run1206@gmail.com>
2026-07-21 19:51:33 -0400

    Change from methods to expressions (.net 10)

--- PlatformService/DotNet10/Data/PlatformRepo.cs
+++ PlatformService/DotNet10/Data/PlatformRepo.cs
@@ -1,31 +1,20 @@
 using PlatformService.Models;
 
-namespace PlatformService.Data
+namespace PlatformService.Data;
+
+public class PlatformRepo(AppDbContext context) : IPlatformRepo
 {
-    public class PlatformRepo(AppDbContext context) : IPlatformRepo
+    private readonly AppDbContext _context = context;
+
+    public void CreatePlatform(Platform platform)
     {
-        private readonly AppDbContext _context = context;
-
-        public void CreatePlatform(Platform platform)
-        {
-            ArgumentNullException.ThrowIfNull(platform);
-
-            _context.Platforms.Add(platform);
-        }
-
-        public IEnumerable<Platform> GetAllPlatforms()
-        {
-            return [.. _context.Platforms];
-        }
-
-        public Platform GetPlatformById(int id)
-        {
-            return _context.Platforms.FirstOrDefault(p => p.Id == id)!;
-        }
-
-        public bool SaveChanges()
-        {
-            return _context.SaveChanges() >= 0;
-        }
+        ArgumentNullException.ThrowIfNull(platform);
+        _context.Platforms.Add(platform);
     }
+
+    public IEnumerable<Platform> GetAllPlatforms() => [.. _context.Platforms];
+
+    public Platform GetPlatformById(int id) => _context.Platforms.FirstOrDefault(p => p.Id == id)!;
+
+    public bool SaveChanges() => _context.SaveChanges() >= 0;
 }
