720b852840593702bccfc3e301182eeeb64ea2a3
Drew W. Pennyfeather <run1206@gmail.com>
2026-07-29 00:46:20 -0400

    Implement Docker container for .net10 folder

--- src/PlatformService.Net10/Program.cs
+++ src/PlatformService.Net10/Program.cs
@@ -25,7 +25,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.MapControllers();
 
